70566291c7ab36253e71a0e7bc0ed4aef789c81b
Shiraj Ahmad <starlordali@gmail.com>
2025-10-21 20:56:06 +0530

    DA 21 | Day 4

--- batches/21/week_01_foundations_environment/day_03/query_1.sql
+++ batches/21/week_01_foundations_environment/day_03/query_1.sql
@@ -16,7 +16,30 @@ select * from salary_history;
 set search_path to sales;
 
 -- selecting all the data set orders table
-select * from orders;
+select 				-- select the columns
+	*  				-- * => all / all columns
+from orders;		-- from selectes the table
+
+
+
+--  select only order_id, order_status, total_amount
+select
+	order_id,order_status,total_amount
+from orders;
+
+
+-- select order_status, order_id, total_amount
+select
+	order_status,order_id,total_amount
+from orders;
+
+
+-- select order_status, order_id, total_amount order them accordingly to order_status
+
+select
+	order_status,
+
+
 
 
 
