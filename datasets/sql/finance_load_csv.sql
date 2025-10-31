e9f9052c1affb3b33d3f59cc3b4c32847a96d591
Shiraj Ahmad <starlordali@gmail.com>
2025-10-31 18:30:47 +0530

    Day 9 | Initial Commit

--- datasets/sql/finance_load_csv.sql
+++
@@ -1,3 +0,0 @@
-\c retailmart;
-\copy finance.expenses        FROM 'datasets/csv/finance/expenses.csv'        CSV HEADER;
-\copy finance.revenue_summary FROM 'datasets/csv/finance/revenue_summary.csv' CSV HEADER;
